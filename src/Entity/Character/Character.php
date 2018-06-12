5e0372d78f73439556d49bfb0ae62999cc23fb66
JoDoDo <julien.herrera@fiducialcloud.fr>
2018-06-12 12:37:36 +0200

    Create Character entity

---
+++ src/Entity/Character/Character.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Entity\Character;
+
+use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Core\Annotation\ApiResource;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ApiResource
+ * @ORM\Entity
+ */
+class Character
+{
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    public $id;
+
+    /**
+     * @var string $name A name property - this description will be available in the API documentation too.
+     *
+     * @ORM\Column
+     * @Assert\NotBlank
+     * @Assert\Type("string")
+     */
+    public $name;
+}
